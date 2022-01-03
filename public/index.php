fde997c019555b4659f1d2fc44eeb4fe9e6a4530
Bostjan Skufca Jese <bostjan@skufca.si>
2022-01-03 15:12:37 +0000

    Add PHP opcache status visualization script
    Source:
    https://github.com/rlerdorf/opcache-status
    
    Related to: aokranj/infrastructure#9

--- public/index.php
+++ public/index.php
@@ -4,4 +4,5 @@
 <b>HTTPD/PHP:</b><br />
 <a href="/server-info">Apache: server-info</a><br />
 <a href="/server-status">Apache: server-status</a><br />
-<a href="/phpinfo">PHP: phpinfo()</a><br />
+<a href="/phpinfo.php">PHP: phpinfo()</a><br />
+<a href="/opcache.php">PHP: OPcache status</a><br />
