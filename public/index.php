7e1a85ea1455fedac5ac35d3c99fab0be683ca22
Bostjan Skufca Jese <bostjan@skufca.si>
2022-01-03 14:36:33 +0000

    Add index.php + phpinfo/

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<b>DB:</b><br />
+<a href="/pma/">phpMyAdmin</a><br />
+<br />
+<b>HTTPD/PHP:</b><br />
+<a href="/server-info">Apache: server-info</a><br />
+<a href="/server-status">Apache: server-status</a><br />
+<a href="/phpinfo">PHP: phpinfo()</a><br />
