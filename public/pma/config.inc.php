83fe67c8b997324b5dcb6e8a0e288e5791802c5b
Bostjan Skufca Jese <bostjan@skufca.si>
2022-01-19 01:18:32 +0000

    Prevent 'root' username from being suggested
    This potentially kills remembering the last username used, but will keep it like this until someone complains ;)

--- public/pma/config.inc.php
+++ public/pma/config.inc.php
@@ -33,6 +33,7 @@ $i++;
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
 $cfg['Servers'][$i]['host'] = 'mariadb-svc';
+$cfg['Servers'][$i]['user'] = '';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
@@ -44,6 +45,7 @@ $i++;
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
 $cfg['Servers'][$i]['host'] = '10.0.3.21';
+$cfg['Servers'][$i]['user'] = '';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
